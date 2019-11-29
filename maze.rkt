acb9b45b13b3b45523dd08d88499c42243ea2cf5
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-11-29 16:31:28 +0000

    Update maze.rkt
    added display function

--- maze.rkt
+++ maze.rkt
@@ -1,9 +1,13 @@
 #lang racket
+
 (require "pvector-lens.rkt")
 (require data/pvector)
 (require data/collection)
 (require lens)
 (require fancy-app)
+
+(define (make-grid height width)
+  (make-pvector height (make-pvector width 0)))
 
 (define NORTH 1)
 (define SOUTH 2)
@@ -23,7 +27,7 @@
 (define (maze height width)
   (let carve-passages-from ([cx 0]
 			    [cy 0]
-			    [grid (make-pvector height (make-pvector width 0))])
+			    [grid (make-grid height width)])
     (for/fold ([grid grid])
       	      ([direction (shuffle (list NORTH SOUTH EAST WEST))])
       (define nx (+ cx (hash-ref DX direction)))
@@ -39,4 +43,30 @@
 						   (pvector-ref-nested-lens ny nx) (bitwise-ior _ (opposite-of direction))))]
 	[else grid]))))
 
-(maze (random 1 10) (random 1 10))
+
+(define (no-south-wall? maze y x)
+  (not (= (bitwise-and (lens-view (pvector-ref-nested-lens y x) maze)
+		       SOUTH)
+	  0)))
+
+(define (no-east-wall? maze y x)
+  (not (= (bitwise-and (lens-view (pvector-ref-nested-lens y x) maze)
+		  EAST)
+     0)) )
+
+(define (display-maze maze height width)
+  (displayln (list->string `(#\space ,@(make-list (* width 2) #\_))))
+  (for ([y (in-range height)])
+    (display #\|)
+    (for ([x (in-range width)])
+      (display (if (no-south-wall? maze y x) 
+		 #\space
+		 #\_))
+      (if (no-east-wall? maze y x) 
+	(display (if (no-south-wall? maze y (add1 x))
+		   #\space
+		   #\_))
+	(display #\|)))
+    (display #\newline)))
+
+(display-maze (maze 20 10) 20 10)
